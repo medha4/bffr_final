58e694298e5e857ea09ac51fae3e6316e27aab07
Medha Gupta <medha4@10-16-28-2.dynapool.wireless.nyu.edu>
2022-11-01 19:28:54 -0400

    added docker and routes

--- bffr_final_project/flask_react/src/App.js
+++ bffr_final_project/flask_react/src/App.js
@@ -28,31 +28,49 @@ function App() {
     //end of new line 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
 
-        {/* new line start*/}
-        <p>To get your profile details: </p><button onClick={getData}>Click me</button>
-        {profileData && <div>
-              <p>Profile name: {profileData.profile_name}</p>
-              <p>About me: {profileData.about_me}</p>
-            </div>
-        }
-         {/* end of new line */}
-      </header>
+    //     {/* new line start*/}
+    //     <p>To get your profile details: </p><button onClick={getData}>Click me</button>
+    //     {profileData && <div>
+    //           <p>Profile name: {profileData.profile_name}</p>
+    //           <p>About me: {profileData.about_me}</p>
+    //         </div>
+    //     }
+    //      {/* end of new line */}
+    //   </header>
+    // </div>
+
+    <div>
+    <center>
+    <h1>BFFR</h1><br></br>
+    <ul>
+	    <li><a href="https://bffr.atlassian.net/wiki/spaces/BFFR/pages/1671170/BFFR+Documentation">User Guide</a></li>
+	    <li><a href="/profile.html">Profile</a></li>
+	    <li><a href="/feed.html">Feed</a></li>
+    </ul>
+    <p>Hello World!</p>
+    <input placeholder="Email"></input><br></br>
+    <input placeholder="Password"></input><br></br>
+    <button>Sign Up!</button>
+    <br></br>
+    <p>Already have an account? Log in</p></center>
     </div>
+
+
   );
 }
 
