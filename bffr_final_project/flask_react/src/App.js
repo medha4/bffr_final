b57462a5eaecf497297b0a37b98fa07a8733e23b
shaygyawali <shaygyawali@gmail.com>
2022-11-15 15:43:51 -0500

    auth front end updates

--- bffr_final_project/flask_react/src/App.js
+++ bffr_final_project/flask_react/src/App.js
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 import Component from 'react';
 import axios from "axios";
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
 
 import 'intl-tel-input/build/css/intlTelInput.css';
-import intlTelInput from 'intl-tel-input';
 
-import ReactIntlTelInput from 'react-intl-tel-input-v2';
+import LogIn from './components/LogIn'
+import SignUp from './components/SignUp'
+import PhoneVerification from './components/PhoneVerification'
 
-function App() {
+
+function App(){
 
    // new line start
-  const [profileData, setProfileData] = useState(null)
+  /*const [profileData, setProfileData] = useState(null)
   const [userNumber, setUserNumber] = useState("000-000-0000")
 
   function updateNumber(evt){
@@ -50,70 +52,19 @@ function App() {
     const value = { iso2: 'cn', dialCode: '86', phone: '12345678901' };
    
     const onChange = value => console.log(value);
-    const onReady = (instance, IntlTelInput) => console.log(instance, IntlTelInput);
+    const onReady = (instance, IntlTelInput) => console.log(instance, IntlTelInput);*/
 
   return (
-    <div class="App">
+    <>
+    <Routes>
+      <Route path="/" element={<LogIn />}> </Route>
+      <Route path="sign-up" element={<SignUp />}> </Route>
+      <Route path="phone-verify" element={<PhoneVerification />}> </Route>
 
-        <span class="dot"></span>
-        <span class="dot2"></span>
-        <span class="dot3"></span>
-        <span class="dot4"></span>
+    </Routes>
+    </>
 
-        <div class = "accentBalls">
-          <span class="dot5"></span>
-          <span class="dot6"></span>
-        </div>
-
-
-      <div class="logoHolder">
-          <h1 class = "title"> BFFR </h1>
-          <h2 class = "subtitle"> Friends' Music Now </h2>
-      </div>
-
-      <div class="mainContainer" >
-
-        <div class="loginContainer"> 
-          <form>
-            <h2 class = "numberTxt"> Phone Number </h2>
-
-            <input class = "phoneInput" type="tel" name="name" placeholder="000-000-0000"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="12" onChange={evt => updateNumber(evt)}/>
-
-            <input class = "submitButton" type="submit" value="Log In -->" />
-            <p value={userNumber}></p>
-          </form>
-        </div>
-
-       
-          <h2 class = "createAccountTxt"> 
-            <a href="/signUp.js" style={{textDecoration: 'none'}}>
-          New here? Sign Up
-            </a>
-          </h2>
-
-      </div>
-
-    </div>
-
-   /* <div>
-      <center>
-      <h1>BFFR</h1><br></br>
-      <ul>
-        <li><a href="https://bffr.atlassian.net/wiki/spaces/BFFR/pages/1671170/BFFR+Documentation">User Guide</a></li>
-        <li><a href="/profile.html">Profile</a></li>
-        <li><a href="/feed.html">Feed</a></li>
-      </ul>
-      <p>Hello World!</p>
-      <input placeholder="Email"></input><br></br>
-      <input placeholder="Password"></input><br></br>
-      <button>Sign Up!</button>
-      <br></br>
-      <p>Already have an account? Log in</p>
-      </center>
-    </div>
-*/
-  );
+  )
 }
 
-export default App;
+export default App
